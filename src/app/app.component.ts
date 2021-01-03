de3bf9dda54ec7f35baadbe2b13b849f34b331c9
Shubham-chandurkar <newshubhammm@gmail.com>
2021-01-03 22:32:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appA';
+  title = 'Counter App';
+  name = "Shubham Chandurkar";
+
+  count: number = 0;
+
+  handleIncrease() {
+    this.count = this.count + 1;
+  }
+
+  handleDecrease() {
+    if (this.count > 0) {
+      this.count = this.count - 1;
+    } else {
+      this.handleReset();
+    }
+  }
+
+  handleReset() {
+    this.count = 0;
+  }
+
 }
